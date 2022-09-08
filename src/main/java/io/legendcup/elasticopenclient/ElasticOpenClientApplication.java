11385639e075c968e01313911ba666ed99ad364f
hyungeun <kingwang09@gmail.com>
2022-09-08 12:15:38 +0900

    Initial commit

---
+++ src/main/java/io/legendcup/elasticopenclient/ElasticOpenClientApplication.java
@@ -0,0 +1,13 @@
+package io.legendcup.elasticopenclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticOpenClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticOpenClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/legendcup/elasticopenclient/ElasticOpenClientApplicationTests.java
@@ -0,0 +1,13 @@
+package io.legendcup.elasticopenclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticOpenClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

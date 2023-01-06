bfce4b48ae79ad95f9a83c12ad1ceffdbc070826
hyungeun <kingwang09@gmail.com>
2023-01-06 14:52:33 +0900

    Git SSH Config Test

--- src/main/java/io/legendcup/elasticopenclient/ElasticOpenClientApplication.java
+++ src/main/java/io/legendcup/elasticopenclient/ElasticOpenClientApplication.java
@@ -3,6 +3,7 @@ package io.legendcup.elasticopenclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Git SSH Config Test
 @SpringBootApplication
 public class ElasticOpenClientApplication {
 
